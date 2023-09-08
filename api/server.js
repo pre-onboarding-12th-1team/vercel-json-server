016816326d911c6833b4f52abff07db8db23e188
방충림 <101491870+HWAHAEBANG@users.noreply.github.com>
2023-09-08 20:06:02 +0900

    Update server.js

--- api/server.js
+++ api/server.js
@@ -4,6 +4,12 @@ const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.header('Access-Control-Allow-Headers', '*')
+  next()
+})
+
 server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
